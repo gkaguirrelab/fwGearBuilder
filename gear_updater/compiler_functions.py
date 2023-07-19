edfc267ae7b3d7ba7cadcd2d28a4eb563803117b
ozenc <HuseyinOzenc.Taskin@ pennmedicine.upenn.edu>
2023-07-19 16:01:01 -0400

    Compiler function draft

--- gear_updater/compiler_functions.py
+++ gear_updater/compiler_functions.py
@@ -5,22 +5,81 @@ This script contains the functions which compile matlab code for various
 projects. These functions here are called by the main_builder.py
 '''
 
-def compile_calcCorticalMag(path_to_matlab_documents, output_folder):
+def compile_bayesprf(path_to_matlab_in_documents, bayesprf_gear_folder):
+    ### Compile calcCorticalMag.m function loacted located in forwardModel 
+    cortmag_funcPath = os.path.join(bayesprf_gear_folder, 'cortmag_func')
+    if os.path.exists(cortmag_funcPath):
+        os.system('rm -r %s' % cortmag_funcPath)    
+    os.system('mkdir %s' % cortmag_funcPath)
+        
+    mcc_call = 'mcc -m -R -nodisplay %s -I %s -I %s -a %s -d %s -v' % (os.path.join(path_to_matlab_in_documents,'projects/forwardModelWrapper/code/bayesPRF/calcCorticalMag.m'),
+                                                                       os.path.join(path_to_matlab_in_documents,'toolboxes/freesurferMatlab/matlab/'),
+                                                                       os.path.join(path_to_matlab_in_documents,'projects/forwardModelWrapper/code/utilities'),
+                                                                       os.path.join(path_to_matlab_in_documents,'toolboxes/matlabcentral/TheilSen/'),
+                                                                       cortmag_funcPath)     
+
+    print('Compiling calcCorticalMag.m \n')
+    os.system(mcc_call)
+
+    ### Compile postprocessBayes.m function located in forwardModel 
+    postproc_funcPath = os.path.join(bayesprf_gear_folder, 'postproc_func')
+    if os.path.exists(postproc_funcPath):
+        os.system('rm -r %s' % postproc_funcPath) 
+    os.system('mkdir %s' % postproc_funcPath)
     
-    # Create the output folder if doesn't exist
-    if not os.path.exists(output_folder):
-        os.system("mkdir %s"%output_folder)
+    mcc_call = 'mcc -m -R -nodisplay %s -I %s -I %s -d %s -v' % (os.path.join(path_to_matlab_in_documents,'projects/forwardModelWrapper/code/bayesPRF/postprocessBayes.m'),
+                                                                 os.path.join(path_to_matlab_in_documents,'toolboxes/ciftiMatlab/'),
+                                                                 os.path.join(path_to_matlab_in_documents,'toolboxes/freesurferMatlab/matlab/'),
+                                                                 postproc_funcPath)
     
-    mcc_path = 'mcc'
-    mcc_call1 = '%s -m -R -nodisplay %s -I %s -I %s -a %s -d %s -v'%(mcc_path,
-                                                                     os.path.join(path_to_matlab_documents,'projects/forwardModelWrapper/code/calcCorticalMag.m'),
-                                                                     os.path.join(path_to_matlab_documents,'toolboxes/freesurferMatlab/matlab/'),
-                                                                     os.path.join(path_to_matlab_documents,'projects/forwardModelWrapper/code/utilities'),
-                                                                     os.path.join(path_to_matlab_documents,'toolboxes/matlabcentral/TheilSen/'),
-                                                                     output_folder)
+    print('Compiling postprocessBayes.m \n')
+    os.system(mcc_call)
     
-    print('Compiling calcCorticalMag.m')
-    os.system(mcc_call1)
+    ### Compile renderInferredMaps.m function located in forwardModel 
+    render_funcPath = os.path.join(bayesprf_gear_folder, 'render_func')
+    if os.path.exists(render_funcPath):
+        os.system('rm -r %s' % render_funcPath) 
+    os.system('mkdir %s' % render_funcPath)
+        
+    mcc_call = 'mcc -m -R -nodisplay %s -I %s -I %s -I %s -d %s -v' % (os.path.join(path_to_matlab_in_documents,'projects/forwardModelWrapper/code/bayesPRF/renderInferredMaps.m'),
+                                                                       os.path.join(path_to_matlab_in_documents,'projects/forwardModelWrapper/code/'),
+                                                                       os.path.join(path_to_matlab_in_documents,'projects/forwardModelWrapper/code/utilities/'),
+                                                                       os.path.join(path_to_matlab_in_documents,'toolboxes/freesurferMatlab/matlab/'),
+                                                                       render_funcPath)
+    
+    print('Compiling renderInferredMaps.m \n')
+    os.system(mcc_call)    
+    
+def compile_createSubjectFodMap(path_to_matlab_in_documents, createSubjectFodMap_gear_folder):
+    ### Compile fodMakerWrapper.m function located in fixelTOMEAnalysis
+    adaptConOpt_funcPath = os.path.join(createSubjectFodMap_gear_folder, 'adaptConOpt')
+    if os.path.exists(adaptConOpt_funcPath):
+        os.system('rm -r %s' % adaptConOpt_funcPath)    
+    os.system('mkdir %s' % adaptConOpt_funcPath)
+    
+    mcc_call = 'mcc -m -R -nodisplay %s -I %s -I %s -a %s -d %s -v' % (os.path.join(path_to_matlab_in_documents,'projects/fixelTOMEAnalysis/code/AdaptConOpt_TranAndShi_2015/fodMakerWrapper.m'),
+                                                                       os.path.join(path_to_matlab_in_documents,'projects/fixelTOMEAnalysis/code/AdaptConOpt_TranAndShi_2015/General_Tools/'),
+                                                                       os.path.join(path_to_matlab_in_documents,'projects/fixelTOMEAnalysis/code/AdaptConOpt_TranAndShi_2015/NIfTI_Tools/'),
+                                                                       os.path.join(path_to_matlab_in_documents,'projects/fixelTOMEAnalysis/code/AdaptConOpt_TranAndShi_2015/qpc'),
+                                                                       adaptConOpt_funcPath)
+    
+    print('Compiling fodMakerWrapper.m \n')
+    os.system(mcc_call)
+                                                                       
+    
+    
+  
+    
+  
+    
+  
+    
+  
+    
+  
+    
+  
+    
 
 def compile_forwardModel(path_to_matlab_documents, output_folder):
     
@@ -46,39 +105,6 @@ def compile_forwardModel(path_to_matlab_documents, output_folder):
     
     print('Compiling mainWrapper.m')
     os.system(mcc_call2)
-
-def compile_postprocessBayes(path_to_matlab_documents, output_folder):
-    
-    # Create the output folder if doesn't exist
-    if not os.path.exists(output_folder):
-        os.system("mkdir %s"%output_folder)
-    
-    mcc_path = 'mcc'
-    mcc_call3 = '%s -m -R -nodisplay %s -I %s -I %s -d %s -v'%(mcc_path,
-                                                              os.path.join(path_to_matlab_documents,'projects/forwardModelWrapper/code/postprocessBayes.m'),
-                                                              os.path.join(path_to_matlab_documents,'toolboxes/HCPpipelines/global/matlab/'),
-                                                              os.path.join(path_to_matlab_documents,'toolboxes/freesurferMatlab/matlab/'),
-                                                              output_folder)
-    
-    print('Compiling postprocessBayes.m')
-    os.system(mcc_call3)
-    
-def compile_renderInferredMaps(path_to_matlab_documents, output_folder):
-    
-    # Create the output folder if doesn't exist
-    if not os.path.exists(output_folder):
-        os.system("mkdir %s"%output_folder)
-    
-    mcc_path = 'mcc'
-    mcc_call4 = '%s -m -R -nodisplay %s -I %s -I %s -I %s -d %s -v'%(mcc_path,
-                                                     os.path.join(path_to_matlab_documents,'projects/forwardModelWrapper/code/renderInferredMaps.m'),
-                                                     os.path.join(path_to_matlab_documents,'projects/forwardModelWrapper/code/'),
-                                                     os.path.join(path_to_matlab_documents,'projects/forwardModelWrapper/code/utilities/'),
-                                                     os.path.join(path_to_matlab_documents,'toolboxes/freesurferMatlab/matlab/'),
-                                                     output_folder)
-    
-    print('Compiling renderInferredMaps.m')
-    os.system(mcc_call4)
     
 def compile_regressMotion(path_to_matlab_documents, output_folder):
 
